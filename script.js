ec8232230de809f3fc640f24e9168e85e5789231
mmartirio <111591395+mmartirio@users.noreply.github.com>
2024-05-30 12:47:11 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,41 @@
+//Criação uma animação para a mudança de tela de login para tela de cadastro
+var Cadastrese = document.querySelector('#btnCadastro');
+var login = document.querySelector('.login');
+var Cadastrar = document.querySelector('#btnCadastrar');
+var cadastro = document.querySelector('.cadastro');
+
+Cadastrese = addEventListener('click', function(){
+if (login.style.display == 'block', cadastro.style.display == 'none'){
+    login.style.display = 'none', cadastro.style.display = 'block'
+}
+else {
+    login.style.display = 'block', cadastro.style.display = 'none'
+}
+})
+
+Cadastrar = addEventListener('click', function(){
+    if (cadastro.style.display == 'block', login.style.display == 'none'){
+        cadastro.style.display = 'none', login.style.display = 'block'
+    }
+    else {
+        cadastro.style.display = 'block', login.style.display = 'none';
+    }
+    });
+
+
+    //Validação dos inputs para liberação do botão
+    function ligaButton() {
+        const nome = document.querySelector('#nomeCadastro').value;
+        const sobrenome = document.querySelector('#sobrenomeCadastro').value;
+        const email = document.querySelector('#emailCadastro').value;
+        const senha = document.querySelector('#senhaCadastro').value;
+
+        if(nome && sobrenome && email && senha){
+        document.querySelector('#btnCadastrar').disabled = false;
+        return; 
+    }
+        document.querySelector('#btnCadastrar').disabled = true;
+    }
+   
+
+   
